0efb59ce512e2e7b1d81dd6230a03d58478969c7
viperdhinesh <viper.dhinesh@gmail.com>
2020-07-10 06:35:19 +0530

    stack adt program using class template completed

--- Datastructures general problems Implementations/3 stack implementation/stack.cpp
+++ Datastructures general problems Implementations/3 stack implementation/stack.cpp
@@ -1,3 +1,86 @@
 #include <iostream> 
 
+using namespace std;     
+
+template <typename T>
+class Stack_node{
+
+    public:
+        T data;    
+        Stack_node *link; 
+
+}; /* end of Stack_node */
+
+
+template <typename S>
+class Stack{
+
+    public:
+        Stack_node<S> *top; 
+
+        Stack(){      
+            this->top = nullptr; 
+        }
+
+        void push(S data);       
+        S pop();       
+        void print();   
+
+};  /* end of Stack */
+
+
+template <typename Sc>
+void Stack<Sc>::push(Sc data){      
+    
+    Stack_node<Sc> *newnode = new Stack_node<Sc>(); 
+    newnode->data = data;   
+    newnode->link = this->top; 
+    this->top = newnode; 
+
+}  /* end of Stack::push() */
+
+template <typename Sc>
+Sc Stack<Sc>::pop(){
+    Sc temp_data; 
+
+    if(this->top != NULL){
+        temp_data = this->top->data; 
+        this->top = this->top->link; 
+    }
+
+    return temp_data; 
+}
+
+template <typename Sc>
+void Stack<Sc>::print(){
+
+    Stack_node<Sc> *temp = this->top; 
+
+    while(temp != NULL){    
+        cout << temp->data << "\n";     
+        temp = temp->link; 
+    }
+
+}  /* end of print() */
+
+
+int main(){
+    int popped_data; 
+
+    Stack<int> *s = new Stack<int>();    
+
+    s->push(5);             
+    s->push(6);
+    s->push(7);     
+    s->push(7);     
+
+    popped_data = s->pop();     
+
+    cout << "popped data is : " << popped_data << "\n"; 
+
+    s->print();        
+
+    return 0; 
+
+} /* end of main() function */
 
