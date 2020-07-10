1c7bbc3dfd4bc8ecd2629a5851a382071f1ae0e9
viperdhinesh <viper.dhinesh@gmail.com>
2020-07-10 16:29:46 +0530

    tower of hanoi

---
+++ Datastructures general problems Implementations/5 tower of hanoi/main.cpp
@@ -0,0 +1,39 @@
+/* Towers of hanoi - O(2^n) */
+
+
+#include <iostream>
+
+using namespace std;    
+
+
+void towers(int num_of_disk, char src, char dest, char aux){
+
+    static int step = 0; 
+
+    cout << "Towers " << num_of_disk << src << dest << aux << "\n"; 
+
+    if(num_of_disk==1){
+        cout << step << "Move from " << src << " -> " << dest << "\n"; 
+        ++step;  
+    }else{
+        towers(num_of_disk-1, src, aux, dest);
+        cout << step << "Move from " << src << " -> " << dest << "\n"; 
+        ++step;
+        towers(num_of_disk-1, aux, dest, src);    
+    }
+
+}  /* end of towers() */
+
+
+int main (){
+
+    int num_of_disk; 
+
+    cout << "number of disk "; 
+    cin >> num_of_disk; 
+
+    towers(num_of_disk, 'A', 'C', 'B'); 
+
+
+    return 0;   
+}
