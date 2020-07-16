15d81f18d5322431f1debd3f03363d435b45c863
viperdhinesh <viper.dhinesh@gmail.com>
2020-07-16 20:44:21 +0530

    waiter problem started

---
+++ Datastructures general problems Implementations/11 deque using array in c++/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std; 
+
+
+class DEQueue{
+
+
+
+
+}; 
+
+
+
+int main(){
+
+
+
+    return 0;   
+
+}  /* end of main() function */
---
+++ general problems/1 find prime numbers from 1 to 100.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std; 
+
+// for checking whether single integer is prime or not, the following method 
+// takes O(n)
+bool isPrime(int number){
+
+    bool condition = true;  
+    
+    if(number <= 1){
+        condition = false; 
+    }else{
+        for(int i=2; i<number; i++){
+            if(number % i == 0){
+                condition = false; 
+                break;  
+            }else{
+                continue; 
+            }
+        }
+    }
+
+    return condition;    
+}
+
+// O(sqrt(n));  
+bool isPrimeEfficient(int number){
+
+    bool condition = true;   
+
+    if(number <= 1){
+        condition = false; 
+    }else if(number == 2 || number == 3){
+        condition = true; 
+    }else{
+
+        for(int i=2; i<= sqrt(number); i++){
+            if(number % i == 0){
+                condition = false; 
+                break; 
+            }else{
+                continue; 
+            }
+        }
+    }
+
+    return condition;    
+}
+
+
+int main(){
+
+    cout << boolalpha;    
+
+    vector<int> vec;    
+
+    // for(int i=2; i<10; i++){
+    //     if(isPrime(i)){
+    //         vec.push_back(i);    
+    //     }    
+    // }
+
+    // vector<int>::iterator it; 
+    // it = vec.begin();   
+    // while(it!=vec.end()){
+    //     cout << *it << '\n'; 
+    //     it++;    
+    // }
+
+    for(int i=2; i<100; i++){
+        if(isPrimeEfficient(i)){
+            vec.push_back(i);    
+        }    
+    }
+
+    vector<int>::iterator it; 
+    it = vec.begin();   
+    while(it!=vec.end()){
+        cout << *it << '\n'; 
+        it++;    
+    }
+
+    return 0;    
+}
