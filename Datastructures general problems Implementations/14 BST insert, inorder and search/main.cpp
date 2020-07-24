a92f9594ebb2ae7215493ee930012291f1a2be83
viperdhinesh <viper.dhinesh@gmail.com>
2020-07-24 11:49:42 +0530

    bst completed

--- Datastructures general problems Implementations/14 BST insert, inorder and search/main.cpp
+++ Datastructures general problems Implementations/14 BST insert, inorder and search/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stack>
-
+#include <vector> 
 
 using namespace std;    
 
@@ -20,12 +20,18 @@ class BST{
         Treenode *root; 
         BST(); 
         ~BST();     
-
         void insert(int key); 
+        Treenode* rInsert(Treenode *p, int key);         
         void inorderDisplay(Treenode *p);      
         Treenode* search(int key);   
-        
-};      
+        Treenode* rSearch(Treenode *p, int key); 
+        int height(Treenode *p); 
+        Treenode* inorderPredecessor(Treenode *p);   
+        Treenode* inorderSuccessor(Treenode *p);      
+        Treenode* Delete(Treenode *p, int key);    
+        void createFromPreorderTraversal(vector<int> pre);    
+
+};              
 
 
 BST::BST(){
@@ -76,6 +82,32 @@ void BST::insert(int key){
 }  /* end of BST::insert(int key) */
 
 
+Treenode* BST::rInsert(Treenode *p, int key){
+
+    Treenode *t; 
+
+    if(p == NULL){
+        t = new Treenode();   
+        t->lchild = t->rchild = NULL; 
+        t->data = key;
+        return t; 
+    }
+
+    if(key < p->data){
+        p->lchild = rInsert(p->lchild, key);     
+
+    }else if(key > p->data){
+        p->rchild = rInsert(p->rchild, key); 
+    
+    }
+
+    return p;    
+
+}  /* end of BST::rInsert() */
+
+
+
+
 void BST::inorderDisplay(Treenode *p){   
     
     if(p){
@@ -110,17 +142,162 @@ Treenode* BST::search(int key){
 }  /* end of Treenode* search() */
 
 
+Treenode* BST::rSearch(Treenode *p, int key){
+
+    if(p ==  nullptr){
+        return nullptr;    
+    }
+
+    if(key == p->data){
+        return p;   
+    }else if(key < p->data){
+        return rSearch(p->lchild, key);    
+    }else if(key > p->data){
+        return rSearch(p->rchild, key);     
+    }
+
+}  /* end of Treenode* BST::rSearch() */
+
+
+
+
+int BST::height(Treenode *p){
+
+    int x, y;    
+
+    if(p == nullptr){
+        return 0;   
+    }
+
+    x = height(p->lchild);   
+    y = height(p->rchild);   
+
+    return x > y ? x + 1 : y + 1;    
+
+}  /* end of BST::height */
+
+
+
+Treenode* BST::inorderPredecessor(Treenode *p){  
+
+    while(p && p->rchild != NULL){
+        p = p->rchild; 
+    }
+
+    return p;  
+
+}  /* end of BST::inorderPredessor */
+
+
+Treenode* BST::inorderSuccessor(Treenode *p){
+    
+    while(p && p->lchild != NULL){
+        p = p->lchild;   
+    }
+
+    return p;    
+
+}  /* end of BST::inorderSuccessor() */
+
+
+Treenode* BST::Delete(Treenode *p, int key){
+    Treenode *q;    
+
+    if(p == nullptr){
+        return nullptr;     
+    }
+
+    if(p->lchild == nullptr && p->rchild == NULL && p->data == key){
+        if(p == root){
+            root = nullptr;    
+        }
+
+        delete p;   
+        return nullptr;    
+    }
+
+    if(key < p->data){
+        p->lchild = Delete(p->lchild, key);    
+    }else if(key > p->data){
+        p->rchild = Delete(p->rchild, key);   
+    }else{
+        if(height(p->lchild) > height(p->rchild)){
+            q = inorderPredecessor(p->lchild);   
+            p->data = q->data;   
+            p->lchild = Delete(p->lchild, q->data); 
+        }else{
+            q = inorderSuccessor(p->rchild);    
+            p->data = q->data;    
+            p->rchild = Delete(p->rchild, q->data);    
+
+        }
+
+    }
+
+    return p;    
+
+}  /* end of Treenode* BST::Delete() */
+
+
+
+void BST::createFromPreorderTraversal(vector<int> vec){
+
+    stack<Treenode*> stk;    
+
+    Treenode *p, *t;   
+
+    int i = 0;   
+
+    root = new Treenode();   
+    root->lchild = root->rchild = nullptr;    
+    root->data = vec[i++];   
+
+    p = root;    
+
+    while(i < vec.size()){   
+
+        if(vec[i] < p->data){
+            t = new Treenode();    
+            t->lchild = t->rchild = nullptr;    
+            t->data = vec[i];
+            i++;         
+            p->lchild = t;    
+            stk.push(p);
+            p = t;    
+        }else if(vec[i] > p->data && vec[i] < stk.empty() ? 32767 : (stk.top())->data){
+            t = new Treenode();   
+            t->lchild = t->rchild = nullptr;   
+            t->data = vec[i];
+            i++;      
+            p->rchild = t; 
+            p = t; 
+        }else{
+            p = stk.top();    
+            stk.pop(); 
+        }
+
+
+    }  /* end of while(i < vec.size()) */
+
+}  /* end of BST::createFromPreorderTraversal */
+
+
 
 
 int main(){
 
     BST t; 
 
+    vector<int> vec{50, 10, 20, 30, 60};     
+
+    t.createFromPreorderTraversal(vec);    
+
     t.insert(50); 
     t.insert(10);
     t.insert(20);
     t.insert(30);
     t.insert(60);   
+    t.insert(100);   
 
     cout << "\nInorder display\n";  /* expected output 10, 20, 30, 50, 60 */ 
     t.inorderDisplay(t.root);         
