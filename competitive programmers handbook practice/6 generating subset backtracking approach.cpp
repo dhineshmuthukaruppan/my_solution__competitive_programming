faf808fffabec25829fe90c42ce58148e802e2ad
viperdhinesh <viper.dhinesh@gmail.com>
2020-08-01 16:46:02 +0530

    generate subsets another method

--- competitive programmers handbook practice/6 generating subset backtracking approach.cpp
+++ competitive programmers handbook practice/6 generating subset backtracking approach.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <string>
 
 using namespace std;   
 
@@ -29,10 +29,51 @@ void generateSubsetRecursionStackApproach(int k, int n){
 }  /* end of generateSubsetRecursionStackApproach */
 
 
+void generateSubsetString(string &s, vector<char> &subset, int stringPosition, int subsetPosition){   
+
+    if(stringPosition == s.size()){
+        // print subset 
+
+        for(int i = 0; i<subset.size(); i++){
+            cout << subset[i];   
+        }
+        cout << '\n';    
+    }else{
+
+        generateSubsetString(s, subset, stringPosition + 1, subsetPosition);          
+        
+        subset[subsetPosition] = s[stringPosition];    
+        generateSubsetString(s, subset, stringPosition + 1, subsetPosition + 1);        
+
+        // subset[subsetPosition] = s[stringPosition];   
+        // generateSubsetString(s, subset, stringPosition + 1, subsetPosition + 1);          
+        
+        // subset[subsetPosition] = ' ';   
+        // generateSubsetString(s, subset, stringPosition + 1, subsetPosition + 1);  
+        
+        /*  abc
+            ab 
+            a c
+            a  
+            bc
+            b 
+            c 
+        */
+    }
+
+
+}  /* end of generateSubsetString() */
+
+
 
 int main(){
 
-    generateSubsetRecursionStackApproach(0, 3);        
+    generateSubsetRecursionStackApproach(0, 3);       
+
+
+    string s = "dhinesh"; 
+    vector<char> subset(s.size());    
+    generateSubsetString(s, subset, 0, 0);            
 
     return 0; 
 
