f310cb8e8b5a55c107167cb5721ad56db5a762ce
viperdhinesh <viper.dhinesh@gmail.com>
2020-08-16 13:41:46 +0530

    hash table two number sum

--- general problems/3 two number sum E.cpp
+++ general problems/3 two number sum E.cpp
@@ -26,6 +26,32 @@ bool bruteforceTwonumberSum(vector<int> &a, int sum, int &result1, int &result2)
 
 
 
+bool hashtableTwonumberSum(vector<int> &a, int sum, int &result1, int &result2){
+
+    map<int, int> mp;    
+
+    for(int i=0; i<a.size(); i++){
+        mp[a[i]] = 1;    
+    }
+
+    for(int i=0; i<a.size(); i++){
+        int diff = sum - a[i];   
+        if(diff == a[i]){
+            continue;     
+        }
+        auto it = mp.find(diff);   
+        if(it != mp.end()){
+            result1 = a[i]; 
+            result2 = diff;
+            return true;             
+        }
+    }
+
+    return false;    
+
+}  /* end of hashtableTwonumberSum() */
+
+
 
 int main(){
 
@@ -35,7 +61,8 @@ int main(){
     // Time - O(n^2)
     cout << bruteforceTwonumberSum(a, 10, result1, result2) << ' ' <<result1 << ' ' << result2 << '\n';           
 
-
+    result1 = -1; result2 = -1;     
+    cout << hashtableTwonumberSum(a, 13, result1, result2) << ' ' <<result1 << ' ' << result2 << '\n';    
 
 
     return 0;   
