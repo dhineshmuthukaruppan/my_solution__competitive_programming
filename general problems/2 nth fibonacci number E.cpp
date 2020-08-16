4a21f94c8bd294285ce574d786096256dd836801
viperdhinesh <viper.dhinesh@gmail.com>
2020-08-16 12:25:51 +0530

    nth fibonacci completed

--- general problems/2 nth fibonacci number E.cpp
+++ general problems/2 nth fibonacci number E.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;    
 
-
+// Time - O(n) Space O(n)
 int nthFibonacci(int n, vector<int> &memoize){
 
     if(n == 1){
@@ -22,16 +22,40 @@ int nthFibonacci(int n, vector<int> &memoize){
 
 } /* end of nthFibonacci() */
 
+// Time - O(n) Space O(1)
+int nthFibonacciOptimized(int n){
+
+    int Track[2] {0, 1};   
+
+    int counter = 3;    
+
+    while(counter <= n){   
+        int fibSum = Track[0] + Track[1];
+        Track[0] = Track[1];   
+        Track[1] = fibSum;    
+        counter++;   
+    }
+
+    if(n < 1){
+        return -1;   
+    }else if(n <=2){
+        return Track[n];
+    }else{
+        return Track[1];
+    }
+        
+}  /* end of nthFibonacciOptimized */
 
 
 int main(){
 
     // Time - O(n) Space O(n)
     vector<int> memoize(11, -1);  // size of 11 and initialize everything to -1
-    cout << nthFibonacci(10, memoize);         
+    cout << nthFibonacci(10, memoize) << '\n';         
 
+    // Time - O(n) Space O(1)
+    cout << nthFibonacciOptimized(10);     
 
-    
 
 
 
