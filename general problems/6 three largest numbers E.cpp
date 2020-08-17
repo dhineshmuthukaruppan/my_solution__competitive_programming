4a206ea4ce747944252e717b97e4b581fc22f101
viperdhinesh <viper.dhinesh@gmail.com>
2020-08-17 21:05:02 +0530

    three largest number priority queue method

--- general problems/6 three largest numbers E.cpp
+++ general problems/6 three largest numbers E.cpp
@@ -6,6 +6,31 @@
 using namespace std;    
 
 
+vector<int> threeLargestNumberPriorityQueue(vector<int> array){
+
+    priority_queue<int, vector<int>, greater<int>> pq;     
+
+    for(int i=0; i<array.size(); i++){
+        if(i < 3){
+            pq.emplace(array[i]); 
+        }else{
+            if(array[i] > pq.top()){
+                pq.pop(); 
+                pq.emplace(array[i]); 
+            }
+        }
+    }
+
+    vector<int> result;    
+    while(!pq.empty()){
+        result.push_back(pq.top());  
+        pq.pop();      
+    }
+
+    return result;     
+
+}  /* END OF threeLargestNumberPriorityQueue() */
+
 
 
 
@@ -13,7 +38,14 @@ int main(){
 
     vector<int> array {141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7}; 
 
-    
+    vector<int> result = threeLargestNumberPriorityQueue(array);    
+
+    cout << "Three largest number\n";   
+    for(int i=0; i<result.size(); i++){
+        cout << result[i] << ' ';
+    }
+
+
 
 
     return 0;  
