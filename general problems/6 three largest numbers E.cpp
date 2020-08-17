0f0fff80e28222ca17f792d2f14ebe00ed6659a8
viperdhinesh <viper.dhinesh@gmail.com>
2020-08-17 20:29:42 +0530

    three largest number setup

---
+++ general problems/6 three largest numbers E.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+
+using namespace std;    
+
+
+
+
+
+int main(){
+
+    vector<int> array {141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7}; 
+
+    
+
+
+    return 0;  
+
+}  /* end of main() */
