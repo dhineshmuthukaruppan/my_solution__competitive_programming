810a0dad9506889254089c936ddcbf45d14b6cdd
viperdhinesh <viper.dhinesh@gmail.com>
2020-08-22 17:29:38 +0530

    max min

--- special problems/1 arrays/2 max min 150.cpp
+++ special problems/1 arrays/2 max min 150.cpp
@@ -22,3 +22,32 @@ int main(){
 
 }  /* end of main() */
 
+
+
+
+/* 
+
+vector<int> find(vector<int> &A,int l,int r)
+{       vector<int> v(2,0);
+    if(l==r)
+    {
+        v[0]=v[1]=A[l];
+        return v;
+    }
+    if(l+1==r)
+    {
+        v[0]=min(A[l],A[r]);
+        v[1]=max(A[l],A[r]);
+        return v;
+    }
+    int mid=l+(r-l)/2;
+    vector<int> L = find(A,l,mid-1); 
+    vector<int> R =find(A,mid,r);
+    v[0]=min(L[0],R[0]);
+    v[1]=max(L[1],R[1]);
+    return v;
+    
+    
+}
+
+ */
