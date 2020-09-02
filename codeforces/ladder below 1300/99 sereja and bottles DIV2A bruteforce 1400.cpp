10957b327bdd52c36301cce1fd6ffde28863f442
viperdhinesh <viper.dhinesh@gmail.com>
2020-09-02 16:15:19 +0530

    sereja and bottles

---
+++ codeforces/ladder below 1300/99 sereja and bottles DIV2A bruteforce 1400.cpp
@@ -0,0 +1,334 @@
+/* ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡖⠁⠀⠀⠀⠀⠀⠀⠈⢲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⢀⣀⣤⣤⣤⣤⣀⡀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣔⢿⡿⠟⠛⠛⠻⢿⡿⣢⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣷⣤⣀⡀⢀⣀⣤⣾⣿⣿⣿⣷⣶⣤⡀⠀⠀⠀⠀
+⠀⠀⢠⣾⣿⡿⠿⠿⠿⣿⣿⣿⣿⡿⠏⠻⢿⣿⣿⣿⣿⠿⠿⠿⢿⣿⣷⡀⠀⠀
+⠀⢠⡿⠋⠁⠀⠀⢸⣿⡇⠉⠻⣿⠇⠀⠀⠸⣿⡿⠋⢰⣿⡇⠀⠀⠈⠙⢿⡄⠀
+⠀⡿⠁⠀⠀⠀⠀⠘⣿⣷⡀⠀⠰⣿⣶⣶⣿⡎⠀⢀⣾⣿⠇⠀⠀⠀⠀⠈⢿⠀
+⠀⡇⠀⠀⠀⠀⠀⠀⠹⣿⣷⣄⠀⣿⣿⣿⣿⠀⣠⣾⣿⠏⠀⠀⠀⠀⠀⠀⢸⠀
+⠀⠁⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⢇⣿⣿⣿⣿⡸⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠈⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠐⢤⣀⣀⢀⣀⣠⣴⣿⣿⠿⠋⠙⠿⣿⣿⣦⣄⣀⠀⠀⣀⡠⠂⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠉⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠋⠁⠀⠀⠀ 
+
+Author name : Dhinesh Muthukaruppan
+
+Description (optional)
+======================
+*/
+
+#include <iostream>  
+#include <vector>  
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <cassert>
+#include <complex>
+#include <random>
+#include <bitset>
+#include <ctime>
+#include <chrono>
+
+using namespace std;    
+
+#define forn(i, n) for(int i = 0; i < int(n); i++) 
+#define fore(i, l, r) for(int i = int(l); i < int(r); i++)
+
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+
+#define pb push_back
+#define mp make_pair
+#define sqr(a) ((a) * (a))    
+#define sz(a) int(a.size())     
+#define all(x) (x).begin(), (x).end()
+#define sortall(x) sort(all(x))
+#define itr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+
+typedef long long ll; 
+typedef long double ld;    
+
+typedef pair<int, int> pi; 
+typedef pair<ll, ll> pl;   
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pi>		vpi;   
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+template <class A, class B>
+ostream& operator << (ostream &out, const pair<A, B> &a);
+
+template <class A>
+ostream& operator <<(ostream &out, const vector<A> &v);
+
+void swap(int &a, int &b);
+vi sieveOfEratosthenes(int n);  // O(n log logn)      
+bool isPrime(int number);  // O(sqrt(n));  
+vi returnAllFactors(int n);
+int totalNumberOfDigits(int A);
+int extractIthDigit(int A, int i);  /* Extract ith digit - (A / 10 ^ i) % 10 */
+int reverseDigits(int A);
+vi allPrimeFactors(int A);   // TODO   
+int gcdByPrimeFactors(int A, int B); // TODO
+int gcdByLoop(int A, int B);   
+int gcdEuclidean(int A, int B);  
+
+
+int solve(){
+
+    return 0;   
+}  /* end of solve() */
+
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  
+
+    int n;   
+    cin >> n;   
+    map<int, int> mp1;   
+    map<int, pi> mp2; 
+    vvi a(n, vi (2));                     
+
+    for(int i=0; i<n; i++){
+        cin >> a[i][0];
+        cin >> a[i][1];    
+
+        if(a[i][0] == a[i][1]){
+            // push it onto mp2
+            mp2[a[i][1]].first++;   
+            mp2[a[i][1]].second = i;        
+
+        }else{
+            // push it onto mp1
+            mp1[a[i][1]]++;   
+        }
+    }  /* end of for i loop */
+    int answer = 0;  
+    for(int i=0; i<n; i++){
+        // if a[i][0] present in mp1 atleast one time or greater than 1 time in mp2 then the bottle can be opened else increment the count
+        if(mp1[a[i][0]] > 0 || mp2[a[i][0]].first > 1){
+            continue;      
+        }else if(mp2[a[i][0]].first == 1 && mp2[a[i][0]].second != i){
+            continue;        
+        }else{
+            answer++;   
+        }         
+    }
+
+    cout << answer << '\n';   
+
+
+/*     int h[1001],a[1001],b[1001];
+        int main(){int i,n,c=0;scanf("%d",&n);
+        for(i=0;i<n;i++){
+            scanf("%d%d",&a[i],&b[i]);
+            h[b[i]]++;}
+            for(i=0;i<n;i++){
+                if(h[a[i]]==0||(h[a[i]]==1&&a[i]==b[i]))c++;}
+                printf("%d\n",c);return 0;}
+
+
+        #include<iostream>
+        using namespace std;
+        int i,n,j,ans,a[105],b[105];
+        int main(){
+        cin>>n;
+        while(i<n){
+            cin>>a[i]>>b[i]; i++;
+        }
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+            if(a[i]==b[j]&&i!=j){ans++;break;}
+        
+            }
+        }cout<<n-ans;
+        }
+
+
+ */
+
+    return 0;   
+
+}  /* end of main() */
+
+
+
+
+
+
+template <class A, class B>
+ostream& operator << (ostream &out, const pair<A, B> &a){
+    return out << "(" << a.x << ", " << a.y << ")";   
+}
+
+template <class A>
+ostream& operator <<(ostream &out, const vector<A> &v){
+    out << "["; 
+    forn(i, sz(v)){
+        if(i) out << ", "; 
+        out << v[i];   
+    }    
+    return out << "]";   
+}
+
+void swap(int &a, int &b){    
+    int temp;    
+    temp = a;   
+    a = b;   
+    b = temp;   
+}  /* end of swap() */
+
+/* O(nloglogn) */
+vi sieveOfEratosthenes(int n){
+    vi primes(n+1, 1);   
+    vi resultVector;     
+    primes[0] = 0;  
+    primes[1] = 0;   
+    for(int i=2; i <= sqrt(n); i++){   
+        if(primes[i] == 1){  
+            for(int j = i * i; j <= n; j+=i){   
+                primes[j] = 0;     
+            }   
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(primes[i] == 1){
+            resultVector.push_back(i);    
+        }
+    }
+    return resultVector;   
+}  /* end of sieveOfEratosthenes() */
+
+// O(sqrt(n));  
+bool isPrime(int number){
+    bool condition = true;   
+    if(number <= 1){
+        condition = false; 
+    }else if(number == 2 || number == 3){
+        condition = true; 
+    }else{
+        for(int i=2; i<= sqrt(number); i++){
+            if(number % i == 0){
+                condition = false; 
+                break; 
+            }else{
+                continue; 
+            }
+        }
+    }
+    return condition;    
+}
+
+vi returnAllFactors(int n){
+    vi result;      
+    for(int i=1; i<=sqrt(n); i++){ // this will be efficient than n or n/2
+        if(n%i == 0){
+            result.pb(i);  
+            if(i != sqrt(n)){
+                result.pb(n/i);     
+            }
+        }
+    }
+    sortall(result);    
+    return result;   
+}  /* end of returnAllFactors() */
+
+int totalNumberOfDigits(int A){
+    int result = 0;   
+    while(A > 0){
+        A = A / 10;   
+        result++;   
+    }    
+    return result;    
+}  /* end of totalNumberOfDigits() */
+
+/* Extract ith digit - (A / 10 ^ i) % 10 */
+int extractIthDigit(int A, int i){
+    int result = (int(A / pow(10, i)) % 10);  
+    return result;            
+} /* end of extractIthDigit() */
+
+int reverseDigits(int A){
+    bool negative_number = false;    
+    if(A < 0){
+        negative_number = true;    
+        A = A * -1;    
+    }    
+    long long reverse = 0;       
+    int prev_reverse = 0;     
+    while(A > 0){
+        int current_digit = A % 10;   
+        reverse = reverse * 10 + current_digit;      
+        if((reverse - current_digit)/10 != prev_reverse){
+            // overflowed
+            return 0;   
+        }
+        prev_reverse = reverse;    
+        A = A / 10;   
+
+    }   
+    if(negative_number && -reverse < INT32_MIN) return 0;   
+    else if(reverse > INT32_MAX) return 0;   
+    else return negative_number ? -reverse :reverse;        
+}  /* end of reverse() */    
+
+vi allPrimeFactors(int A){
+    vi result;   
+
+    return result;  
+}  /* end of allPrimeFactors() */  
+
+int gcdByPrimeFactors(int A, int B){
+    int result;  
+
+    return result;   
+}  /* end of gcdByPrimeFactors() */
+
+int gcdByLoop(int A, int B){
+    A = (A > 0) ? A : -A;   
+    B = (B > 0) ? B : -B;   
+    while(A != B){
+        if(A == 0) return B;   
+        if(B == 0) return A;   
+        if(A > B) A -= B;   
+        else if(B > A) B-= A;   
+    }
+    return A;   
+}  /* end of gcdByLoop() */
+
+int gcdEuclidean(int A, int B){
+    if(B == 0)
+        return A;   
+    return gcdEuclidean(B, A % B);       
+}  /* end of gcdEuclidean() */  
+
+
+
+
+
+
+
+
+
+
+
