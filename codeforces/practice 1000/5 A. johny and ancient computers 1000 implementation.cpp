5465a610c7460f63435966759011915c188e2187
viperdhinesh <viper.dhinesh@gmail.com>
2020-10-04 18:17:54 +0530

    johny and ancient computer

--- codeforces/practice 1000/5 A. johny and ancient computers 1000 implementation.cpp
+++ codeforces/practice 1000/5 A. johny and ancient computers 1000 implementation.cpp
@@ -137,6 +137,35 @@ void solve(){
 }  /* end of solve() */
 
 
+/* 
+LL getR(LL a){
+	while(a % 2 == 0)
+		a /= 2;
+	return a;
+}
+
+void solve(){
+	LL a, b;
+	scanf("%lld %lld", &a, &b);
+	if(a > b)	swap(a, b);
+	
+	LL r = getR(a);
+	if(getR(b) != r){
+		puts("-1");
+		return;
+	}
+	
+	int ans = 0;
+	b /= a;
+	
+	while(b >= 8)
+		b /= 8, ++ans;
+	if(b > 1)	++ans;
+	printf("%d\n", ans);
+}
+
+ */
+
 
 int main(){
 
