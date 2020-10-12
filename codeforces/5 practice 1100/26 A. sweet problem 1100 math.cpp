76bc54cc07f0b2ab10d70ac796f1d21fe7269e1f
viperdhinesh <viper.dhinesh@gmail.com>
2020-10-12 14:42:32 +0530

    sweet problem

--- codeforces/5 practice 1100/26 A. sweet problem 1100 math.cpp
+++ codeforces/5 practice 1100/26 A. sweet problem 1100 math.cpp
@@ -88,6 +88,26 @@ ll f(ll x){
 }
 
 
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// #define forn(i, n) for (int i = 0; i < int(n); i++)
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     forn(tt, t) {   
+//         int a[3];
+//         cin >> a[0] >> a[1] >> a[2];
+//         sort(a, a + 3);
+//         if (a[2] <= a[0] + a[1])
+//             cout << (a[0] + a[1] + a[2]) / 2 << endl;
+//         else
+//             cout << a[0] + a[1] << endl;
+//     }
+// }
+
 void solve(){
 
     ll t, n, m, l, r, w, x, y, z, k, temp, answer, mini, maxi, miniIndex, maxiIndex, counter, sum;  
