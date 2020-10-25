14b72324c64cdad78c662dc08e0b89ade980f445
viperdhinesh <viper.dhinesh@gmail.com>
2020-10-25 19:20:07 +0530

    buying shovels completed

--- codeforces/7 practice 1300/16 D. buying shovels 1300 number theory.cpp
+++ codeforces/7 practice 1300/16 D. buying shovels 1300 number theory.cpp
@@ -99,28 +99,47 @@ void solve(){
 
         cin >> n >> k;   
 
-        cond = true; 
-        if(k >= n){
-            cout << 1 << newl;    
-        }else{
+        // cond = true; 
+        // if(k >= n){
+        //     cout << 1 << newl;    
+        // }else{
 
-            for(int i=2; i*i<=n; i++){
-                if(i > k){
-                    break; 
-                }
-                if(n%i == 0){
-                    cout << i << newl; 
-                    cond = false; 
-                    break;
+        //     for(int i=2; i*i<=n; i++){
+        //         if(i > k){
+        //             break; 
+        //         }
+        //         if(n%i == 0 && (n/i) <= k){    
+        //             cout << i << newl; 
+        //             cond = false; 
+        //             break;
+        //         }
+
+        //     }
+
+        //     if(cond){
+        //         cout << n << newl; 
+        //     }   
+
+        // }
+
+
+        answer = n;    
+
+        for(int i=1; i*i<=n; i++){
+
+            if(n%i == 0){
+                if(i<=k){
+                    answer = min(answer, n/i);     
                 }
 
+                if(n/i <= k){
+                    answer = min(answer, static_cast<ll>(i));        
+                }
             }
 
-            if(cond){
-                cout << n << newl; 
-            }   
-
         }
+
+        cout << answer << newl; 
 
 
         // if(cond){cout << "YES" << newl; }else{cout << "NO" << newl; }  
