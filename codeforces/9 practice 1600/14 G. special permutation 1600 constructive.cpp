7902df3623eba182ed108cc2131b52e059ac7065
viperdhinesh <viper.dhinesh@gmail.com>
2020-11-24 19:58:24 +0530

    special permutation completed

--- codeforces/9 practice 1600/14 G. special permutation 1600 constructive.cpp
+++ codeforces/9 practice 1600/14 G. special permutation 1600 constructive.cpp
@@ -98,6 +98,20 @@ void solve(){
     // t = 1;        
     while(t--){          
 
+        cin >> n;   
+
+        if(n < 4){
+            cout << -1 << newl;  
+        }else{
+            for(int i=n; i>=1; i--){
+                if(i&1){cout << i << " "; }
+            }
+            cout << 4 << " " << 2 << " ";  
+            for(int i=6; i<=n; i+=2){
+                cout << i << " ";  
+            }
+            cout << newl; 
+        }
 
 
 
